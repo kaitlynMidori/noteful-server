dd21b18d39ea051c12230dc310f5fa4c4afb1216
Justin Hager <coachjustinhager@gmail.com>
2020-12-13 11:19:03 -0800

    adds seed files

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (id, folder_name)
+VALUES
+    (1, 'first folder'),
+    (2, 'second folder'),
+    (3, 'third folder');
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (id, note_name, content, folder_id)
+VALUES
+    (1, 'first note', 'some content', 1),
+    (2, 'second note', 'some content', 2),
+    (3, 'third note', 'some content', 3);
