611be90a70c47684e8196894ef1e06a718416f8c
Justin Hager <coachjustinhager@gmail.com>
2020-12-13 13:40:34 -0800

    fixes router issue. adds more folder routes and tests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
-// const someRouter = require('./some-routers/some-router');
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const foldersRouter = require('./folders/folders-router')
 
-const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 const app = express();
 app.use(morgan(morganOption));
@@ -14,18 +14,18 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// app.use("/anendpoint", someRouter);
+app.use('/api/folders', foldersRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!')
+  res.send(`Active endpoints: '/api/folders' and '/api/notes'`)
 });
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
   } else {
-    console.error("error");
+    console.error('error');
     response = { message: error.message, error };
   }
   res.status(500).json(response);
