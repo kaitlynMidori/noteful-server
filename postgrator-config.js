4af2715590701332f446397543b9d6d0c4121c2d
Justin Hager <coachjustinhager@gmail.com>
2020-12-16 13:14:07 -0800

    using nvm and node 12.14

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 
- const {DATABASE_URL}  = require('./src/conf')
-
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-  "ssl": process.env.NODE_ENV === 'production'
 };
 
