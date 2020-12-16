4b1a519d0392dc6428f1af861a4ce14f3042b663
Justin Hager <coachjustinhager@gmail.com>
2020-12-16 12:45:31 -0800

    update DATABASE_URL logic

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+ const {DATABASE_URL}  = require('./src/config')
+
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL
+    : DATABASE_URL
 };
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
+  
+  DATABASE_URL: process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? '?ssl=true' : '') || 'postgresql://postgres@localhost/noteful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test'
 };
+// DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
