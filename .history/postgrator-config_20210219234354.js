55188a9ff7eb11d3fbed870fdc102b94eb51b785
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-02-19 23:55:53 -0800

    ssl edit

--- .history/postgrator-config_20210219234354.js
+++ .history/postgrator-config_20210219234354.js
@@ -6,10 +6,6 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-    // "ssl": (process.env.NODE_ENV === 'production')
+    "ssl": (process.env.NODE_ENV === 'production')
 };
 
-// const client = new Client({
-//   connectionString: connectionString,
-//   ssl: { rejectUnauthorized: false }
-// })
