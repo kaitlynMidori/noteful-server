560371b7e72e4e3758076e55e46fd03dd813b37b
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-02-22 21:10:09 -0800

    .env edit

---
+++ .history/postgrator-config_20210222205212.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+module.exports = {
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+    "ssl": (process.env.NODE_ENV === 'production')
+};
+
+// const client = new Client({
+//   connectionString: connectionString,
+//   ssl: { rejectUnauthorized: false }
+// })
+
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
